ef8fc376c9ce4897d2dac71556b007bf26cb7ccb
Adrian Peng <apeng2@hawaii.edu>
2023-11-15 21:37:28 -1000

    Updated NavBar for ICS Classes link

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
               <Nav.Link className="px-3" id="home-nav" as={NavLink} to="/home" key="home">Home</Nav.Link>,
               <Nav.Link className="px-3" id="create-meeting-nav" as={NavLink} to="/createmeeting" key="createmeeting">Create Meeting</Nav.Link>,
               <Nav.Link className="px-3" id="calendar-nav" as={NavLink} to="/calendar" key="calendar">Calendar</Nav.Link>,
-              <Nav.Link className="px-3" id="ics-section-nav" as={NavLink} to="/icssection" key="icssection">ICS Sections</Nav.Link>,
+              <Nav.Link className="px-3" id="ics-clases-nav" as={NavLink} to="/class" key="icsclass">ICS Classes</Nav.Link>,
               <Nav.Link className="px-3" id="user-profile-nav" as={NavLink} to="/userprofile" key="userprofile">User Profile</Nav.Link>,
             ]) : ''}
             {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
