376f45f3b7184ef7d8e9054663267d930f42aa0d
braydens02 <braydens@hawaii.edu>
2023-12-10 16:53:40 -1000

    added a student and course collection

---
+++ app/imports/api/student/Student.js
@@ -0,0 +1,32 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The StudentsCollection. It encapsulates state and variable values for students.
+ */
+class StudentsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'StudentsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      firstName: String,
+      lastName: String,
+      username: String,
+      picture: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the StuffsCollection.
+ * @type {StudentsCollection}
+ */
+export const Students = new StudentsCollection();
