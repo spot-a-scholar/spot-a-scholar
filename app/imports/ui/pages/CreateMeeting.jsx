91a43db39547982da08b45a7b9beb9b1e2900110
braydens02 <braydens@hawaii.edu>
2023-12-10 23:11:45 -1000

    finished adding functionality to create meeting

--- app/imports/ui/pages/CreateMeeting.jsx
+++ app/imports/ui/pages/CreateMeeting.jsx
@@ -1,35 +1,60 @@
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import {
-  AutoForm, SubmitField, DateField, LongTextField, TextField,
-} from 'uniforms-bootstrap5';
+import { AutoForm, SubmitField, LongTextField, TextField } from 'uniforms-bootstrap5';
 import React from 'react';
+import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { MeetingInfoSchema as formSchema } from '../forms/MeetingInfo';
+import SimpleSchema from 'simpl-schema';
+import { Meetings } from '../../api/meeting/Meetings';
+
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  courseCode: String,
+  location: String,
+  sessionYear: String,
+  sessionMonth: String,
+  sessionDay: String,
+  sessionTime: String,
+  description: String,
+  owner: String,
+});
 
 const bridge = new SimpleSchema2Bridge(formSchema);
-// temp function to get rid of errors, need to implement actual submit function
-const submit = (data, formRef) => data + formRef;
 const CreateMeeting = () => {
-/* Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  /* Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+
+  const submit = (data, formRef) => {
+    const { courseCode, location, sessionYear, sessionMonth, sessionDay, sessionTime, description, owner } = data;
+    Meetings.collection.insert(
+      { courseCode, location, sessionYear, sessionMonth, sessionDay, sessionTime, description, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      },
+    );
+  }
+
   let fRef = null;
   return (
     <Container>
       <Row className="justify-content-center">
         <Col>
           <h2 className="text-center">Create Meeting</h2>
-          <AutoForm ref={(ref) => { fRef = ref; }} schema={bridge} onSubmit={(data) => submit(data, fRef)}>
+          <AutoForm ref={(ref) => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
             <Card className="p-2">
-              <Row>
-                <Col><TextField name="topics" showInlineError placeholder="Topics/Classes" /></Col>
-              </Row>
-              <Row>
-                <Col><DateField name="startTime" showInlineError placeholder="Schedule the Meeting" /></Col>
-                <Col><DateField name="endTime" showInlineError placeholder="Description" /></Col>
-              </Row>
-              <Row>
-                <Col><LongTextField name="description" showInlineError placeholder="Description" /></Col>
-                <SubmitField value="Submit" />
-              </Row>
+              <TextField name="courseCode" />
+              <TextField name="location" />
+              <TextField name="sessionYear" />
+              <TextField name="sessionMonth" />
+              <TextField name="sessionDay" />
+              <TextField name="sessionTime" />
+              <LongTextField name="description" />
+              <TextField name="owner" />
+              <SubmitField value="Submit" />
             </Card>
           </AutoForm>
         </Col>
